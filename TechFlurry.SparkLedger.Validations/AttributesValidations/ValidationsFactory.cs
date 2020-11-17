d2918dae2cbc02e50c4304abe01fc4d0a82b3106
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2020-11-17 11:37:47 +0500

    added validations

---
+++ TechFlurry.SparkLedger.Validations/AttributesValidations/ValidationsFactory.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using TechFlurry.SparkLedger.Validations.CommonValidations;
+
+namespace TechFlurry.SparkLedger.Validations.AttributesValidations
+{
+    public static class ValidationsFactory
+    {
+        public static IValidator<T> GetValidator<T>(ValidationAttributes attributeName)
+        {
+            IValidator<T> validator = null;
+            switch (attributeName)
+            {
+                case ValidationAttributes.LedgerAccountTitle:
+                    validator = new LedgerAccountTitleValidator<T>();
+                    break;
+                case ValidationAttributes.AccountHolderPhone:
+                    validator = new AccountHolderPhoneValidator<T>();
+                    break;
+            }
+            return validator;
+        }
+    }
+    public enum ValidationAttributes
+    {
+        LedgerAccountTitle,
+        AccountHolderPhone
+    }
+    class LedgerAccountTitleValidator<T> : AbstractValidator<T>
+    {
+        public LedgerAccountTitleValidator()
+        {
+            RuleFor(x => x).NotNull().NotEmpty().WithMessage("Account Title is required");
+        }
+    }
+    class AccountHolderPhoneValidator<T> : AbstractValidator<T>
+    {
+        public AccountHolderPhoneValidator()
+        {
+            RuleFor(x => x).NotNull().NotEmpty().WithMessage("Account holder's phone is required")
+                            .SetValidator((IValidator<T>)PhoneNumberValidator.GetValidator());
+        }
+    }
+}
---
+++ TechFlurry.SparkLedger.Validations/CommonValidations/Validator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using System.Linq;
+
+namespace TechFlurry.SparkLedger.Validations.CommonValidations
+{
+
+    internal static class PhoneNumberValidator
+    {
+        public static IValidator<string> GetValidator()
+        {
+            return new Validator();
+        }
+        class Validator : AbstractValidator<string>
+        {
+            public Validator()
+            {
+                RuleFor(x => x).Length(13).WithMessage("Phone number is too short")
+                                .Must(x =>
+                                {
+                                    return !string.IsNullOrEmpty(x) && x.Substring(0, 1) == "+";
+                                })
+                                .Must(x =>
+                                {
+                                    return !string.IsNullOrEmpty(x) && x.Substring(1).All(y => y >= 48 && y <= 57);
+                                }).WithMessage("Not a valid phone number (e.g. +923XXXXXXXXX)");
+            }
+        }
+    }
+}
